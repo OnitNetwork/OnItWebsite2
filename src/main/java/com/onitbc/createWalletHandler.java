bc71241639f3b40cb42cad66595dfc623cf0a988
BunchieFarms <bohmer@uncc.edu>
2018-04-08 02:44:23 -0400

    Deleted login and createwallet jsp file, replaced with servlets, working keygen on create wallet

---
+++ src/main/java/com/onitbc/createWalletHandler.java
@@ -0,0 +1,161 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.onitbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.security.*;
+import java.security.spec.ECGenParameterSpec;
+
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+import org.bouncycastle.util.encoders.Base64;
+
+/**
+ *
+ * @author bryce
+ */
+@WebServlet(name = "createWalletHandler", urlPatterns = {"/createWalletHandler"})
+public class createWalletHandler extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param s
+     * @param request servlet request
+     * @param response servlet response
+     * @return 
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */        
+    
+    /*public String getKeyPair(String s) {
+        KeyGen.generateKeyPair();
+        String response = "oops";
+        
+        if ( null != s ) switch (s) {
+            case "puKey":
+                response = new String(Base64.encode(KeyGen.publicKey.getEncoded()));
+                break;
+            case "prKey":
+                response = new String(Base64.encode(KeyGen.privateKey.getEncoded()));
+                break;
+            case "puAdd":
+                response = HashGen.translate(new String(Base64.encode(KeyGen.publicKey.getEncoded())));
+                break;
+            default:
+                break;
+        }
+        return response;
+    }*/
+    
+    public String getKeyPair() {
+        KeyGen.generateKeyPair();
+        String puKey = new String(Base64.encode(KeyGen.publicKey.getEncoded()));
+        String prKey = new String(Base64.encode(KeyGen.privateKey.getEncoded()));
+        String puAdd = HashGen.translate(new String(Base64.encode(KeyGen.publicKey.getEncoded())));
+        
+        String keys = "<p style=\"margin: -5px auto -10px auto;\">Public:</p><br>"
+                + "<label><p style=\"word-wrap: break-word; font-size: 15px;\">" 
+                + new String(Base64.encode(KeyGen.publicKey.getEncoded())) 
+                + "</p></label><br><br>"
+                + "<p style=\"margin: -10px auto -10px auto;\">Private:</p><br>"
+                + "<label><p style=\"word-wrap: break-word; font-size: 15px;\">"
+                + new String(Base64.encode(KeyGen.privateKey.getEncoded()))
+                + "</p></label><br><br>";
+        return keys;
+    }
+    
+    /*public String getPrivateKey() {
+        KeyGen.generateKeyPair();
+        String prKey = "<p>" + new String(Base64.encode(KeyGen.privateKey.getEncoded())) + "</p>";        
+        return prKey;
+    }
+    
+    public String getPublicKey() {
+        String puKey = new String(Base64.encode(KeyGen.publicKey.getEncoded()));
+        return puKey;
+    }
+    
+        String puAdd = HashGen.translate(new String(Base64.encode(KeyGen.publicKey.getEncoded())));*/
+    
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>OnIt - Create New Wallet</title>");
+            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\">");
+            out.println("<meta charset=\"UTF-8\">");
+            out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            out.println("</head>");
+            out.println("<body>");
+
+            out.println("<div class=\"login-box\">");
+            out.println("<img src=\"img/wallet1.png\" class=\"logo\">");
+
+            out.println("<form action=\"transaction.jsp\" method=\"POST\">");
+            out.println(getKeyPair());
+            out.println("<h2>COPY THIS DOWN</h2>");
+            out.println("<p><input type=\"submit\" value=\"Continue\"></p>");
+            out.println("</form>");
+            out.println("</div>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/main/java/com/onitbc/loginHandler.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.onitbc;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author bryce
+ */
+@WebServlet(name = "loginHandler", urlPatterns = {"/loginHandler"})
+public class loginHandler extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {        
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+        try (PrintWriter out = response.getWriter()) {
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>OnIt - Login</title>");
+            out.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"css/style.css\">");
+            out.println("<meta charset=\"UTF-8\">");
+            out.println("<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">");
+            out.println("</head>");
+            out.println("<body>");
+
+            out.println("<div class=\"login-box\">");
+            out.println("<img src=\"img/login.png\" class=\"logo\">");
+
+            out.println("<form action=\"transaction.jsp\" method=\"POST\">");
+            out.println("<p>Private Key:</p>");
+            out.println("<input type=\"text\" name=\"public\">");                
+            out.println("<p><input type=\"submit\" value=\"Login\"></p>");            
+            out.println("</form>");
+            out.println("</div>)");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
