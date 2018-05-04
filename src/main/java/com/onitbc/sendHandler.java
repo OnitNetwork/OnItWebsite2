a9a52e38ef2a7982e1e6b023995a0c35c19f23d6
BunchieFarms <bohmer@uncc.edu>
2018-05-04 07:35:10 -0400

    changed sql update statement used to update sender's balance, should work now

--- src/main/java/com/onitbc/sendHandler.java
+++ src/main/java/com/onitbc/sendHandler.java
@@ -143,7 +143,7 @@ public class sendHandler extends HttpServlet {
                     
                     conn.setAutoCommit(false);
                     
-                    st.executeUpdate("update users set balance = " + senderFinalBalance + " where pubaddr = '" + privateKey + "';");
+                    st.executeUpdate("update users set balance = " + senderFinalBalance + " where prikey = '" + privateKey + "';");
                     st.executeUpdate("update users set balance = " + recipientFinalBalance + " where pubaddr = '" + sendAddr + "';");
                     st.executeUpdate("insert into transactions (amount_sent, hash, sending_addr, receiving_addr) values (" + sendAmount +
                             ", " + "'" + hashed + "', 'none', '" + sendAddr + "');");
